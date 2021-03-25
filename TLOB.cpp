f83d97c667a9e8ae545aceb34c6c3d4b46fd3e32
Hapiel <daniel@danielsiegmund.nl>
2021-03-25 13:44:51 +0100

    New: debounce, next/prev, allOn/allOff, blink/stop, update

--- TLOB.cpp
+++ TLOB.cpp
@@ -1,31 +1,39 @@
 #include "Arduino.h"
 #include "TLOB.h"
 
-TLOB::TLOB(int led1Pin, int led2Pin, int led3Pin, int buttonPin){
+TLOB::TLOB(int led0Pin, int led1Pin, int led2Pin, int buttonPin){
+  pinMode(led0Pin, OUTPUT);
   pinMode(led1Pin, OUTPUT);
   pinMode(led2Pin, OUTPUT);
-  pinMode(led3Pin, OUTPUT);
+  _led0Pin = led0Pin;
   _led1Pin = led1Pin;
   _led2Pin = led2Pin;
-  _led3Pin = led3Pin;
   _buttonPin = buttonPin;
+  // default debounce time
+  debounceTime = 15;
+  holdTimer = 0;
 }
 
 void TLOB::updateButton(){
   // update button on the beginning of each step. 
-  // QUESTION: Could end work just as well, so that we only need a single update function?
+
   // TODO: add debouncing
 
   // false by default
-  buttonReleased = true;
+  buttonReleased = false;
   buttonPressed = false;
   buttonHold = 0;
+
+  // get state ASAP so all reads are definitely the same, except if within debounce time.
+  if (holdTimer + debounceTime < millis()){
+    buttonState = digitalRead(_buttonPin);
+  }
 
   // no button down registered yet
   if (!buttonDown) {
 
     // but physical button is down
-    if (digitalRead(_buttonPin)) {
+    if (buttonState) {
       buttonPressed = true;
 
       // record time
@@ -34,11 +42,8 @@ void TLOB::updateButton(){
 
   } else {
 
-    // button down was already registered, so were no longer in pressed event.
-    buttonPressed = false;
-
     // if button is still down
-    if (digitalRead(_buttonPin)) {
+    if (buttonState) {
       // time since time recorded
       buttonHold = millis() - holdTimer;
 
@@ -49,13 +54,75 @@ void TLOB::updateButton(){
   }
 
   // set buttonDown to physical button state
-  buttonDown = digitalRead (_buttonPin);
+  buttonDown = buttonState;
+  
 }
 
 void TLOB::updateLeds(){
   // update leds at end of each step.
-  // QUESTION: do we also want individual led variabes, led1, led2, led3?
-  digitalWrite( _led1Pin, leds[0]);
-  digitalWrite( _led2Pin, leds[1]);
-  digitalWrite( _led3Pin, leds[2]);
+  
+  // blinking
+  for (int led = 0; led < 3; led++){
+    if (blinking[led]){
+      if ((millis() - blinkStart[led]) % (blinkOn[led] + blinkOff[led]) < blinkOn[led]){
+        leds[led] = true;
+      } else {
+        leds[led] = false;
+      }
+    }
+  }
+  
+  // update the leds
+  digitalWrite( _led0Pin, leds[0]);
+  digitalWrite( _led1Pin, leds[1]);
+  digitalWrite( _led2Pin, leds[2]);
+}
+
+void TLOB::update(){
+  updateButton();
+  updateLeds();
+}
+
+void TLOB::allOn(){
+  leds[0] = true;
+  leds[1] = true;
+  leds[2] = true;
+}
+
+void TLOB::allOff(){
+  leds[0] = false;
+  leds[1] = false;
+  leds[2] = false;
+}
+
+int TLOB::next(int n){
+  return (n + 1) % 3;
+}
+
+int TLOB::prev(int n){
+  return (n + 2) % 3;
+}
+
+void TLOB::blink(int led, unsigned int onTime, unsigned int offTime){
+  // starts blinking, optionally specify the time led stays on and off. Always starts on.
+  if (offTime == 0){
+    offTime = onTime;
+  }
+  blinking[led] = true;
+  blinkOn[led] = onTime;
+  blinkOff[led] = offTime;
+  blinkStart[led] = millis();
+}
+    
+    
+void TLOB::stop(int led){
+  //stops the blinking.
+  blinking[led] = false;
+}
+
+void TLOB::allStop(){
+  //stops all blinking.
+  blinking[0] = false;
+  blinking[1] = false;
+  blinking[2] = false;
 }
--- TLOB.h
+++ TLOB.h
@@ -15,15 +15,29 @@ class TLOB {
     bool buttonReleased;
     unsigned long buttonHold;
     bool leds[3];
+    int debounceTime;
     void updateButton();
     void updateLeds();
+    void update();
+    void allOn();
+    void allOff();
+    void allStop();
+    int next(int n);
+    int prev(int n);
+    void blink(int led, unsigned int onTime = 100, unsigned int offTime = 0);
+    void stop(int led);
+    bool blinking[3];
+    unsigned int blinkOn[3];
+    unsigned int blinkOff[3];
 
   private:
-  int _led1Pin;
-  int _led2Pin;
-  int _led3Pin;
-  int _buttonPin;
-  unsigned long holdTimer;
+    int _led0Pin;
+    int _led1Pin;
+    int _led2Pin;
+    int _buttonPin;
+    bool buttonState;
+    unsigned long holdTimer;
+    unsigned long blinkStart[3];
 };
 
 #endif
