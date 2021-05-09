a217ad8f9d565c0a68c9a6b107683619d1baa7f9
Hapiel <daniel@danielsiegmund.nl>
2021-05-09 09:23:17 +0200

    ledAll, button functions, restructuring of file

--- TLOB.cpp
+++ TLOB.cpp
@@ -14,8 +14,117 @@ TLOB::TLOB(int led0Pin, int led1Pin, int led2Pin, int buttonPin){
   holdTimer = 0;
 }
 
+ // starts blinking, optionally specify the time led stays on and off. Always starts on.
+
+void TLOB::blink(int led, unsigned int onTime, unsigned int offTime){
+
+  // if no offTime is entered, offTime and onTime are equal.
+  if (offTime == 0){
+    offTime = onTime;
+  }
+
+  // check for valid led selection
+  if (led >= 0 && led <= 2){
+    // turn on blinking, the turning on and off of the leds is handled in updateleds()
+    blinking[led] = true;
+    blinkOn[led] = onTime;
+    blinkOff[led] = offTime;
+    blinkStart[led] = millis();
+  }
+}
+
+void TLOB::blinkAll(unsigned int onTime, unsigned int offTime){
+  blink(0, onTime, offTime);
+  blink(1, onTime, offTime);
+  blink(2, onTime, offTime);
+}
+
+// stops the blinking, does not turn the led off
+void TLOB::stop(int led){
+  // check for valid led selection
+  if (led >= 0 && led <= 2){
+    blinking[led] = false;
+  }
+}
+
+void TLOB::stopAll(){
+  // stops all blinking.
+  blinking[0] = false;
+  blinking[1] = false;
+  blinking[2] = false;
+}
+
+
+// read the button values, these are updated in updateButton()
+
+// is the button down
+bool TLOB::buttonDown(){
+  return buttonDown;
+}
+
+// has the button been pressed since the last updateButton(), debounced
+bool TLOB::buttonPressed(){
+  return buttonPressed;
+}
+
+// has the button been released since the last updateButton(), debounced or delayed if pressed within the debounce time
+bool TLOB::buttonReleased(){
+  return buttonReleased;
+}
+
+// how long has the button been held. 0 if not down
+unsigned long TLOB::buttonHold(){
+  return buttonHold;
+}
+
+// change the debounce time
+int TLOB::debounceTime(unsigned int time){
+  // default of time is 0
+  if(time){
+    debounceTime = time;
+  }
+  return debounceTime;
+}
+
+bool TLOB::led(int led, bool state){
+  // set led
+  if (led >= 0 && led <= 2){
+    digitalWrite(ledPins[led], state);
+    return leds[led] = state;
+  }
+}
+
+bool TLOB::led(int led){
+  // read led state
+  if (led >= 0 && led <= 2){
+    return leds[led];
+  }
+}
+
+void TLOB::ledAll(bool state){
+  led(0, state);
+  led(1, state);
+  led(2, state);
+}
+
+// select next number, wraps around from 2 to 0.
+int TLOB::next(int n){
+  return (n + 1) % 3;
+}
+
+// select prevous number, wraps around from 0 to 2.
+int TLOB::prev(int n){
+  return (n + 2) % 3;
+}
+
+// update both button & leds
+void TLOB::update(){
+  updateButton();
+  updateLeds();
+}
+
+// update button, recommended to do at the beginning or end of every loop
 void TLOB::updateButton(){
-  // update button on the beginning of each step. 
 
   // false by default
   buttonReleased = false;
@@ -56,8 +165,8 @@ void TLOB::updateButton(){
   
 }
 
+// update the leds for blinking, recommended to do at the end of every loop
 void TLOB::updateLeds(){
-  // update leds at end of each step, like when using the leds[] array or when blinking.
   
   // blinking
   for (int led = 0; led < 3; led++){
@@ -75,101 +184,3 @@ void TLOB::updateLeds(){
   digitalWrite( ledPins[1], leds[1]);
   digitalWrite( ledPins[2], leds[2]);
 }
-
-// update both button & leds
-void TLOB::update(){
-  updateButton();
-  updateLeds();
-}
-
-void TLOB::allOn(){
-  led(0, true);
-  led(1, true);
-  led(2, true);
-}
-
-// alias
-void TLOB::onAll(){
-  allOn();
-}
-
-void TLOB::allOff(){
-  led(0, false);
-  led(1, false);
-  led(2, false);
-}
-
-// alias
-void TLOB::offAll(){
-  allOff();
-}
-
-int TLOB::next(int n){
-  return (n + 1) % 3;
-}
-
-int TLOB::prev(int n){
-  return (n + 2) % 3;
-}
-
-void TLOB::blink(int led, unsigned int onTime, unsigned int offTime){
-  // starts blinking, optionally specify the time led stays on and off. Always starts on.
-  if (offTime == 0){
-    offTime = onTime;
-  }
-
-  // I had a bug in code when blink(-1,100) was accidentally entered. Lets check for odd numbers, is that the best fix?
-  if (led >= 0 && led <= 2){
-    blinking[led] = true;
-    blinkOn[led] = onTime;
-    blinkOff[led] = offTime;
-    blinkStart[led] = millis();
-  }
-}
-
-void TLOB::blinkAll(unsigned int onTime, unsigned int offTime){
-  blink(0, onTime, offTime);
-  blink(1, onTime, offTime);
-  blink(2, onTime, offTime);
-}
-
-// alias
-void TLOB::allBlink(unsigned int onTime, unsigned int offTime){
-  blinkAll(onTime, offTime);
-}
-    
-void TLOB::stop(int led){
-  //stops the blinking.
-  if (led >= 0 && led <= 2){
-    blinking[led] = false;
-  }
-}
-
-void TLOB::stopAll(){
-  //stops all blinking.
-  blinking[0] = false;
-  blinking[1] = false;
-  blinking[2] = false;
-}
-
-// alias
-void TLOB::allStop(){
-  stopAll();
-}
-
-int TLOB::led(int led, bool state){
-  // set led
-  if (led >= 0 && led <= 2){
-    digitalWrite(ledPins[led], state);
-    return leds[led] = state;
-  }
-}
-
-int TLOB::led(int led){
-  // read led state
-  if (led >= 0 && led <= 2){
-    return leds[led];
-  }
-}
-
-// const int light = TLOB::led;
--- TLOB.h
+++ TLOB.h
@@ -1,5 +1,5 @@
 /*
-  TLOB 3l1b 3 lights 1 button WIP library
+  TLOB 3L1B 3 leds 1 button library 0.1
 */
 
 #ifndef TLOB_h
@@ -10,41 +10,46 @@
 class TLOB {
   public:
     TLOB(int led0Pin, int led1Pin, int led2Pin, int buttonPin);
-    bool buttonDown;
-    bool buttonPressed;
-    bool buttonReleased;
-    unsigned long buttonHold; // time a button is down
-    bool leds[3];
-    int debounceTime;
-    void updateButton();
-    void updateLeds();
-    void update();
-    void allOn();
-    void allOff();
-    void onAll(); // alias
-    void offAll(); // alias
-    int next(int n);
-    int prev(int n);
+
     void blink(int led, unsigned int onTime = 100, unsigned int offTime = 0);
     void blinkAll(unsigned int onTime = 100, unsigned int offTime = 0);
-    void allBlink(unsigned int onTime = 100, unsigned int offTime = 0); // alias
     void stop(int led);
     void stopAll();
-    void allStop(); // alias
-    bool blinking[3];
-    unsigned int blinkOn[3];
-    unsigned int blinkOff[3];
-    int led(int led, bool state);
-    int led(int led);
-    // Question: how to add alias?
-    //const int light;
+
+    bool buttonDown();
+    bool buttonPressed();
+    bool buttonReleased();
+    unsigned long buttonHold();
+
+    int debounceTime(unsigned int time = 0);
+
+    bool led(int led, bool state);
+    bool led(int led);
+    void ledAll(bool state);
+
+    int next(int n);
+    int prev(int n);
+
+    void update();
+    void updateButton();
+    void updateLeds();
+
 
   private:
+    int debounceTime;
     int ledPins[3];
     int _buttonPin;
     bool buttonState;
     unsigned long holdTimer;
     unsigned long blinkStart[3];
+    bool buttonDown;
+    bool buttonPressed;
+    bool buttonReleased;
+    unsigned long buttonHold;
+    bool leds[3];
+    bool blinking[3];
+    unsigned int blinkOn[3];
+    unsigned int blinkOff[3];
 };
 
 #endif
